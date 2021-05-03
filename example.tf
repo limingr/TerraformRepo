61425dfe9c5d2fcfedafd01be82004a22d6660f7
Liming Ren <limingrms@gmail.com>
2021-05-03 16:53:03 -0700

    Remote org and Terracloud backend.

---
+++ example.tf
@@ -0,0 +1,48 @@
+/* terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+
+  backend "remote" {
+    organization = "TerraformRen"
+    workspaces {
+        name = "tfc-getting-started"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
+ */
+
+ terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+
+ backend "remote" {
+   organization = "TerraformRen"
+   workspaces {
+     name = "MyTerraformRepo"
+   }
+ }
+ 
+}
+
+provider "azurerm" {
+  features {}
+}
