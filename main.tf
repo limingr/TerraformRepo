8d5a2e9d05a700ed95873a1a9feae45296b7b8e8
unknown <limingrms@gmail.com>
2021-05-09 16:47:10 -0700

    Added terraform and dir. 0447

--- main.tf
+++ main.tf
@@ -7,12 +7,12 @@ terraform {
     }
   }
 
-  backend "azurerm" {
+ /*  backend "azurerm" {
     resource_group_name   = "tstate"
     storage_account_name  = "tstate13126"
     container_name        = "tstate"
     key                   = "terraform.mytfstate"
-  }
+  } */
 
   required_version = ">= 0.14.9"
 }
