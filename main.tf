8d49875b2e4d3777e731d0b57d2ce78eb0ca4b28
unknown <limingrms@gmail.com>
2021-05-21 19:01:29 -0700

    Variable name prefix.

--- main.tf
+++ main.tf
@@ -181,10 +181,6 @@ output "current_subscription_display_name" {
   value = data.azurerm_subscription.current.display_name
 }
 
-output "current_subscription_display_name" {
-  value = data.azurerm_subscription.current.display_name
-}
-
 data "azurerm_client_config" "current" {
 }
 
